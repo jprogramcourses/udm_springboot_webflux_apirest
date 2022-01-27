e495913fd438d0094826c06bb5ecc26ba8aafa46
jprogramcourses <jprogramcourses@gmail.com>
2022-01-27 14:32:12 +0100

    Creación de crud con RouterFunction

--- src/main/java/com/bolsadeideas/springboot/webflux/app/RouterFunctionConfig.java
+++ src/main/java/com/bolsadeideas/springboot/webflux/app/RouterFunctionConfig.java
@@ -1,18 +1,59 @@
 package com.bolsadeideas.springboot.webflux.app;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RequestPredicate;
+import org.springframework.web.reactive.function.server.RequestPredicates;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
+import com.bolsadeideas.springboot.webflux.app.handler.ProductoHandler;
+import com.bolsadeideas.springboot.webflux.app.models.services.ProductoService;
+
 @Configuration
 public class RouterFunctionConfig {
 	
+	@Autowired
+	private ProductoService service;
+	
+//	@Bean
+//	public RouterFunction<ServerResponse> routes(){
+//		
+//		/**
+//		 * Los parámetros que acepta el método route son:
+//		 * <ul>
+//		 * <li>RequestPredicate: Mapea la ruta url</li>
+//		 * <li>HandlerFunction: Implementación de la respuesta del request</li>
+//		 * </ul>
+//		 * 
+//		 * Clase de ejemplo incluyendo el handler.
+//		 */
+//		return RouterFunctions.route(RequestPredicates.GET("/api/v2/productos").or(RequestPredicates.GET("/api/v3/productos")), request -> {
+//			return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON)
+//					.body(service.findAll(), Producto.class);
+//		});
+//	}
+	
+	/**
+	 * Clase de ejemplo desacoplando el handler.
+	 * 
+	 * @param handler
+	 * @return
+	 */
 	@Bean
-	public RouterFunction<ServerResponse> routes(){
+	public RouterFunction<ServerResponse> routesWithHandler(ProductoHandler handler){
+		return RouterFunctions
+				.route(RequestPredicates.GET("/api/v2/productoshandler"), request -> handler.listar(request))
+				.andRoute(RequestPredicates.GET("/api/v2/productos/{id}"), request -> handler.ver(request))
+				.andRoute(RequestPredicates.POST("/api/v2/productos"), request -> handler.crear(request))
+				.andRoute(RequestPredicates.PUT("/api/v2/productos/{id}"), handler::modificar)
+				.andRoute(RequestPredicates.DELETE("/api/v2/productos/{id}"), handler::eliminar)
+				.andRoute(RequestPredicates.POST("/api/v2/productos/upload/{id}"), request -> handler.upload(request))
+				.andRoute(RequestPredicates.POST("/api/v2/productos/crearconfoto"), request -> handler.crearConFoto(request));
 		
-		return RouterFunctions.route(null, null)
 	}
 
 }
--- src/main/java/com/bolsadeideas/springboot/webflux/app/handler/ProductoHandler.java
+++ src/main/java/com/bolsadeideas/springboot/webflux/app/handler/ProductoHandler.java
@@ -1,10 +1,157 @@
 package com.bolsadeideas.springboot.webflux.app.handler;
 
-import org.springframework.stereotype.Component;
+import java.io.File;
+import java.net.URI;
+import java.util.Date;
+import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.http.codec.multipart.FilePart;
+import org.springframework.http.codec.multipart.FormFieldPart;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import com.bolsadeideas.springboot.webflux.app.models.documents.Categoria;
+import com.bolsadeideas.springboot.webflux.app.models.documents.Producto;
+import com.bolsadeideas.springboot.webflux.app.models.services.ProductoService;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+/**
+ * Hace las funciones de controlador.
+ * 
+ * @author juand
+ *
+ */
 @Component
 public class ProductoHandler {
+
+	private ProductoService service;
 	
+	private Validator validator;
 	
+	@Value("${config.uploads.path}")
+	private String path;
+
+	public ProductoHandler(ProductoService service, Validator validator) {
+		this.service = service;
+		this.validator = validator;
+	}
+
+	public Mono<ServerResponse> listar(ServerRequest request) {
+		return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).body(service.findAll(), Producto.class);
+	}
+
+	public Mono<ServerResponse> ver(ServerRequest request) {
+		String id = request.pathVariable("id");
+		return service.findById(id).flatMap(
+				p -> ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).body(BodyInserters.fromObject(p)))
+				.switchIfEmpty(ServerResponse.notFound().build());
+	}
+
+	public Mono<ServerResponse> crear(ServerRequest request) {
+		Mono<Producto> producto = request.bodyToMono(Producto.class);
+
+		return producto.flatMap(p -> {
+			
+			// Store information about the validation of the object
+			Errors errors = new BeanPropertyBindingResult(p, Producto.class.getName());
+					
+			validator.validate(p, errors);
+			
+			if(errors.hasErrors()) {
+				return Flux.fromIterable(errors.getFieldErrors())
+						.map(fieldError -> "El campo " + fieldError.getField() + " " + fieldError.getDefaultMessage())
+						.collectList()
+						.flatMap(list -> ServerResponse.badRequest().body(BodyInserters.fromObject(list)));
+			}else {
+				if (p.getCreateAt() == null) {
+					p.setCreateAt(new Date());
+				}
+				return service.save(p).flatMap(pdb -> ServerResponse
+						.created(URI.create("/api/v2/productos/".concat(pdb.getId())))
+						.contentType(MediaType.APPLICATION_JSON)
+						.body(BodyInserters.fromObject(pdb)));
+			}
+			
+			
+		});
+	}
+	
+	public Mono<ServerResponse> modificar(ServerRequest request){
+		String id = request.pathVariable("id");
+		Mono<Producto> monoProductoNuevo = request.bodyToMono(Producto.class);
+		
+		Mono<Producto> monoProductoExistente = service.findById(id);
+		
+		return monoProductoExistente.zipWith(monoProductoNuevo, (prExist, prNuevo) -> {
+			prExist.setNombre(prNuevo.getNombre());
+			prExist.setPrecio(prNuevo.getPrecio());
+			prExist.setCategoria(prNuevo.getCategoria());
+			return prExist;
+		}).flatMap(p -> ServerResponse
+				.created(URI.create("/api/v2/productos/".concat(p.getId())))
+				.contentType(MediaType.APPLICATION_JSON)
+				.body(service.save(p), Producto.class))
+			.switchIfEmpty(ServerResponse.notFound().build());
+	}
+	
+	public Mono<ServerResponse> eliminar(ServerRequest request){
+		String id = request.pathVariable("id");
+		Mono<Producto> productoDb = service.findById(id);
+		
+		return productoDb.flatMap(p -> service.delete(p).then(ServerResponse.noContent().build()))
+				.switchIfEmpty(ServerResponse.notFound().build());
+	}
+	
+	public Mono<ServerResponse> upload(ServerRequest request){
+		String id = request.pathVariable("id");
+		return request.multipartData()
+				.map(multipart -> multipart
+						.toSingleValueMap().get("file")).cast(FilePart.class).flatMap(file -> service.findById(id)
+								.flatMap(p -> {
+									p.setFoto(UUID.randomUUID().toString() + "-" + file.filename()
+									.replace(" ", "-")
+									.replace(":", "")
+									.replace("\\", ""));
+									return file.transferTo(new File(path + p.getFoto())).then(service.save(p));
+		})).flatMap(p -> ServerResponse.created(URI.create("/api/v2/productos/".concat(p.getId())))
+				.contentType(MediaType.APPLICATION_JSON).body(BodyInserters.fromObject(p)))
+		.switchIfEmpty(ServerResponse.notFound().build());
+	}
+	
+	public Mono<ServerResponse> crearConFoto(ServerRequest request){
+		
+		Mono<Producto> producto = request.multipartData().map(multipart ->{
+			FormFieldPart nombre = (FormFieldPart) multipart.toSingleValueMap().get("nombre");
+			FormFieldPart precio = (FormFieldPart) multipart.toSingleValueMap().get("precio");
+			FormFieldPart categoriaId = (FormFieldPart) multipart.toSingleValueMap().get("categoria.id");
+			FormFieldPart categoriaNombre = (FormFieldPart) multipart.toSingleValueMap().get("categoria.nombre");
+			
+			Categoria categoria = new Categoria(categoriaNombre.value());
+			categoria.setId(categoriaId.value());
+			return new Producto(nombre.value(), Double.parseDouble(precio.value()), categoria);
+		});
+		
+		return request.multipartData()
+				.map(multipart -> multipart
+						.toSingleValueMap().get("file")).cast(FilePart.class).flatMap(file -> producto
+								.flatMap(p -> {
+									p.setFoto(UUID.randomUUID().toString() + "-" + file.filename()
+									.replace(" ", "-")
+									.replace(":", "")
+									.replace("\\", ""));
+									p.setCreateAt(new Date());
+									return file.transferTo(new File(path + p.getFoto())).then(service.save(p));
+		})).flatMap(p -> ServerResponse.created(URI.create("/api/v2/productos/".concat(p.getId())))
+				.contentType(MediaType.APPLICATION_JSON).body(BodyInserters.fromObject(p)));
+	}
 
 }
